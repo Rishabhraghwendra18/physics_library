e0d7c713bff1258e7768eade3fe22a15d8be7a29
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-23 22:47:18 +0530

    solo_mag_added
    calculation of magnetic flux through solonoid

--- Physics_library/Physics_library.cpp
+++ Physics_library/Physics_library.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include "framework.h"
 namespace emi {
-	
+#define uo 12.56
+#define tur_len( tu , len ) tu/len
+	float solo_mag(float N, float i )
+	{
+		return uo*N * i;
+	}
 
 }
