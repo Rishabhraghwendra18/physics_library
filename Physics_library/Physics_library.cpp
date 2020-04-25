04e71d4e6dbfd2d6a1ef415beef0730a42bb2f5a
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-25 21:34:32 +0530

    added speed of wave

--- Physics_library/Physics_library.cpp
+++ Physics_library/Physics_library.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "framework.h"
+#include<math.h>
 namespace emi {
 #define uo 12.56
 #define pi 3.14
@@ -8,8 +9,13 @@ namespace emi {
 }
 namespace EMW {
 #define c 3
-	float sp_Ew(float mag) {
+	float sp_Ew(float mag) //speed of Electric field 
+	{
 		return c * mag;
+	}
+	float sp_W(float u, float e)
+	{
+		return 1.0 / sqrt(u * e) ;
 	}
 		
 }
